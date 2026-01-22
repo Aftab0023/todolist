afc624452fb5723b2e3fce47da9d9441264ec78b
Aftab0023 <162977501+Aftab0023@users.noreply.github.com>
2026-01-22 22:56:00 +0530

    Advance todo app created

--- src/TabContainer.jsx
+++ src/TabContainer.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { tabsData } from './Data/tabs'
 
 export default function TabContainer() {
+
+  let [activetab, setActivetab] = useState(0);
+  let [activecontent, setActiveContent] = useState(tabsData[0]);
+  let changeData = (index) => {
+    setActivetab(index)
+    setActiveContent(tabsData[index])
+  }
+
   return (
     <div className='tabsOuter'>
         <h1 style={{textAlign: 'left'}}>Tabbing System </h1>
+        <ul>
+          {tabsData.map((tabsitem, index)=>{
+            return(
+              <li>
+                <button onClick={() => changeData(index)} className={activetab == index ? 'activeButton' : ''}>
+                  {tabsitem.title}
+                </button>
+              </li>
+            )
+          })}
+          
+        </ul>
+
+        {activecontent !== undefined ?
+            <p>
+              {activecontent.description}
+            </p>
+            :
+             ''
+        }
     </div>
     
   )
